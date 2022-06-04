624fa278e83b3ebcd30fe885d692fb17feab065c
Illya Romaniv <mippikzilla@gmail.com>
2022-06-04 11:30:22 +0300

    Updates

--- src/components/assets/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import s from './Button.module.css'
-const CustomButton = (props) => {
-    return (
-        <button className={s.button} {...props}>{props.children}</button>
-    );
-};
-
-export default CustomButton;
