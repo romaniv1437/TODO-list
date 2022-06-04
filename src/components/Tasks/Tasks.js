bc8f9d6c5adeff03d27db46986fd6dd4285366d5
Illya Romaniv <mippikzilla@gmail.com>
2022-06-05 00:10:07 +0300

    Revert "change task text, TaskChangeForm"
    This reverts commit 103759e92c5a7418d84e740f992dee15cac07b2f.

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,29 +1,39 @@
 import React, {useState} from 'react';
 import s from './Tasks.module.css'
-import TaskChangeForm from "./TaskChangeForm/TaskChangeForm";
-import TaskElement from "./TaskElement/TaskElement";
+import {MdDeleteOutline, MdOutlineModeEditOutline} from "react-icons/md";
+import CustomInput from "../assets/Input/Input";
+import CustomButton from "../assets/Button/Button";
 
 const Tasks = (props) => {
     const [newTaskText, setNewTaskText] = useState('')
-    const [onChange, setOnChange] = useState({taskId: 0, isChange: false})
-    const onTaskChange = (taskId, isChange) => {
-        setOnChange({taskId: taskId, isChange: isChange})
-        console.log(onChange)
+    const [onChange, setOnChange] = useState(false)
+    const onTaskChange = () => {
+        setOnChange(!onChange)
     }
+
+    let task_elements = props.tasks.map(task => <div key={task.id} className={s.task_el}>
+                <div className={s.left}>
+                    <p>{task.text}</p>
+                    <span>{task.difficulty}</span>
+                </div>
+                <div className={s.right}>
+                    {onChange ? <div className={s.change_task}>
+                        <CustomInput placeholder='Enter new text' value={newTaskText}
+                                     onChange={(e) => setNewTaskText(e.target.value)}/>
+                        <CustomButton onClick={() => {
+                            props.editTask(task.id, newTaskText);
+                            setNewTaskText('')
+                            onTaskChange(false)
+                        }}>save</CustomButton>
+                    </div> : null}
+                    <MdOutlineModeEditOutline onClick={() => onTaskChange(task.id, true)}/>
+                    <MdDeleteOutline onClick={() => props.removeTask(task.id)}/>
+                </div>
+            </div>)
 
     return (
         <div className={s.tasks}>
-            {onChange.isChange ? <TaskChangeForm onChange={onChange}
-                                                 newTaskText={newTaskText}
-                                                 setNewTaskText={setNewTaskText}
-                                                 onTaskChange={onTaskChange}
-                                                 editTask={props.editTask}/> :
-                <TaskElement tasks={props.tasks}
-                             onTaskChange={onTaskChange}
-                             removeTask={props.removeTask}
-                />
-            }
-
+            {task_elements.length ? task_elements : <h3>Nothing To Do</h3>}
         </div>
     );
 };
